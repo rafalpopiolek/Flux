567491f9251c22e11d2acb5965ba3761a453d74f
rafl00 <rpopiolek2000@gmail.com>
2023-12-08 14:23:02 +0100

    Update packages, add ecs and phpstan

---
+++ ecs.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Phpdoc\PhpdocLineSpanFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    $ecsConfig->sets([
+        SetList::PSR_12,
+        SetList::COMMON,
+        SetList::CLEAN_CODE,
+        SetList::STRICT,
+    ]);
+
+    $ecsConfig->skip([
+        PhpdocLineSpanFixer::class,
+    ]);
+
+    $ecsConfig->parallel();
+};
