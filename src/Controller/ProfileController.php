8d491c0875c3b5712b441e36953ef90cd02aa0c9
rafl00 <rpopiolek2000@gmail.com>
2023-12-19 22:01:59 +0100

    Fix ecs

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Entity\User;
@@ -41,7 +43,7 @@ class ProfileController extends AbstractController
         }
 
         return $this->render('profile/createProfileForm.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
     }
 }
