7e70d13c602dfb53d6655c4784e9bfc14fdf91a1
rafl00 <rpopiolek2000@gmail.com>
2023-12-21 22:11:49 +0100

    Fix ecs

--- src/Form/AddProfileType.php
+++ src/Form/AddProfileType.php
@@ -11,8 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
-
 
 class AddProfileType extends AbstractType
 {
