720cc1c35b4e3f6bdde6118bf24f58bd7cd0d65d
rafl00 <rpopiolek2000@gmail.com>
2023-12-28 18:36:06 +0100

    Fix ecs

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CommentController extends AbstractController
 {
